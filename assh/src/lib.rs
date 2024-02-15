9e2d045e489b5d9f74f45d684214099048d24c76
Leon ROUX <leon.roux@federate.us>
2024-02-15 19:18:39 +0100

    Reorganized the project to be a workspace and provide multiple crates

--- assh/src/lib.rs
+++ assh/src/lib.rs
@@ -1,4 +1,11 @@
-#![doc = include_str!("../README.md")]
+#![doc = concat!(
+    "[![docs.rs](https://img.shields.io/docsrs/", env!("CARGO_PKG_NAME"), ")](https://docs.rs/", env!("CARGO_PKG_NAME"), ")",
+    " ",
+    "[![crates.io](https://img.shields.io/crates/l/", env!("CARGO_PKG_NAME"), ")](https://crates.io/crates/", env!("CARGO_PKG_NAME"), ")"
+)]
+#![doc = ""]
+#![doc = env!("CARGO_PKG_DESCRIPTION")]
+//!
 //! ### Supported algorithms
 //!
 //! #### Key-exchange:
